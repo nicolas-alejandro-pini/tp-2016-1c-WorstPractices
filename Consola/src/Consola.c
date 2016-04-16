a6397bc2dc2dc9cad2d062a0543737ddb9ab3b6f
nicolas-toranzo <nico.toranzo@gmail.com>
2016-04-16 17:39:49 -0300

    Initial commit

---
+++ Consola/src/Consola.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : Consola.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("Console Application"); /* prints Console Application */
+	return EXIT_SUCCESS;
+}
---
+++ Swap/src/Swap.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : Swap.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("Swap Application"); /* prints Swap Application */
+	return EXIT_SUCCESS;
+}
