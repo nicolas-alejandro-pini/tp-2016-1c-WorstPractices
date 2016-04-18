24a08537918b91df072a3d0840acc653203b4084
martinez-ezequiel <ezequiel.martinez@hotmail.com>
2016-04-17 21:50:10 -0300

    Solucion de algunos warnings y nomenclatura.

--- CPU/src/CPU.c
+++ CPU/src/CPU.c
@@ -14,13 +14,44 @@
 #include <arpa/inet.h>
 #include <string.h>
 
+int cpuDameSocket (void){
+	//EML: Las IP y Puerto recibirlo por parametro.
+	struct sockaddr_in direccionServidor;
+		direccionServidor.sin_family = AF_INET;
+		direccionServidor.sin_addr.s_addr = inet_addr("127.0.0.1");
+		direccionServidor.sin_port = htons(8081); //Le indico el puerto en el que escucha.
+
+		int cliente = socket(AF_INET, SOCK_STREAM, 0);
+
+		if(connect(cliente, (void*)&direccionServidor, sizeof(direccionServidor))!=0)
+			return 0;//El connect si devuelve 0 es por un error.
+		else
+			return cliente;
+}
+
+int cpuConectarseAlNucleo(void){
+
+	int idNucleo = 0;
+	puts("Conectando al nucleo...\n");
+	idNucleo = cpuDameSocket();
+	return idNucleo;
+}
+
+int cpuConectarseAlUmc(void){
+
+	int idUMC = 0;
+	puts("Conectando al UMC...\n");
+	idUMC = cpuDameSocket();
+	return idUMC;
+}
+
 int main(void) {
-	int nucleo =0;
-	int umc = 0;
+	int nucleo = 0;
+	//int umc = 0;
 	puts("CPU Application"); /* prints CPU Application */
 
-	nucleo = cpu_conectarse_al_nucleo(); //EML: Pendiente- Leer un .conf y pasar ip y puerto.
-	//umc = cpu_conectarse_al_umc();
+	nucleo = cpuConectarseAlNucleo(); //EML: Pendiente- Leer un .conf y pasar ip y puerto.
+	//umc = cpuConectarseAlUmc();
 
 	if (nucleo != 0){
 		puts("Conectado con el nucleo.");
@@ -35,35 +66,6 @@ int main(void) {
 	return EXIT_SUCCESS;
 }
 
-int cpu_conectarse_al_nucleo(void){
 
-	int idNucleo = 0;
-	puts("Conectando al nucleo...\n");
-	idNucleo = cpu_dame_socket();
-	return idNucleo;
-}
-
-int cpu_conectarse_al_umc(void){
-
-	int idUMC = 0;
-	puts("Conectando al UMC...\n");
-	idUMC = cpu_dame_socket();
-	return idUMC;
-}
-
-int cpu_dame_socket (void){
-	//EML: Las IP y Puerto recibirlo por parametro.
-	struct sockaddr_in dirreccionServidor;
-		dirreccionServidor.sin_family = AF_INET;
-		dirreccionServidor.sin_addr.s_addr = inet_addr("127.0.0.1");
-		dirreccionServidor.sin_port = htons(8081); //Le indico el puerto en el que escucha.
-
-		int cliente = socket(AF_INET, SOCK_STREAM, 0);
-
-		if(connect(cliente, (void*)&dirreccionServidor, sizeof(dirreccionServidor))!=0)
-			return 0;//El connect si devuelve 0 es por un error.
-		else
-			return cliente;
-}
 
 
