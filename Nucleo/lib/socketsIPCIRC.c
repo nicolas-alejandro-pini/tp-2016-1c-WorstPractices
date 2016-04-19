1c479645ecc7510858392cd2f84453a179cbdc96
josemasuarez <josemasuarez@gmail.com>
2016-04-19 02:25:31 -0300

    Commit inicial sin tratamiento de hilos para las escuchas

---
+++ Nucleo/lib/socketsIPCIRC.c
@@ -0,0 +1,117 @@
+/*----------------------------------------------------------------------------*/
+/*                --------------------------------------------                */
+/*                 BIBLIOTECA PARA MANEJO DE SOCKETS EN ANSI C                */
+/*                --------------------------------------------                */
+/*                                                                            */
+/*  Nombre: sockets.c                                                         */
+/*  Versión: 1.0                                                              */
+/*  Fecha: 18 de Abril de 2016					              				  */
+/*  Description: Implementación de la biblioteca para mensajes IPCIRC         */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
+/*
+	#include <dirent.h>
+	#include <signal.h>
+	#include <sys/time.h>
+	#include <sys/stat.h>
+	#include <stdio.h>
+	#include <stdlib.h>
+	#include <string.h>
+	#include <unistd.h>
+	#include <sys/types.h>
+	#include <sys/socket.h>
+	#include <netinet/in.h>
+	#include <arpa/inet.h>
+	#include <errno.h>
+*/
+
+/*----------------------------------------------------------------------------*/
+/*                     Definiciones y Declaraciones                           */
+/*----------------------------------------------------------------------------*/
+
+typedef struct
+{
+	char id[16];
+	char tipo;
+	int largo;
+/*} __attribute__((packed)) stHeaderIPC; */
+} stHeaderIPC; 
+
+typedef struct
+{
+	stHeaderIPC header;
+	char contenido[LONGITUD_MAX_DE_CONTENIDO];/* } __attribute__((packed)) stMensajeIPC;*/
+}stMensajeIPC;	
+
+/*----------------------------------------------------------------------------*/
+/*                         Funciones Privadas                                 */
+/*----------------------------------------------------------------------------*/
+/* Devuelve un nuevo header con los campos cargados. */
+stHeaderIPC nuevoHeaderIPC(const char unTipo){
+	int i;
+	stHeaderIPC unHeader;
+	char *unID = nuevoID();
+	for(i = 0;i <16;i++) unHeader.id[i] = *(unID+i);
+	unHeader.tipo = unTipo;
+	unHeader.largo = 0;
+	return (unHeader);
+}
+
+/*----------------------------------------------------------------------------*/
+/*Envía unHeader por el socket que recibe como parametro y devuelve la cantidad enviada.*/
+int enviarHeaderIPC(int unSocket, const stHeaderIPC unHeader){
+	int resultado;
+
+	if((resultado = send(unSocket, &unHeader, sizeof(stHeaderIPC),0)) == -1)
+		error("No se pudo enviar el header!");
+	return(resultado);
+}
+
+/*----------------------------------------------------------------------------*/
+/*Devuelve el tamaño del header recibido atraves de unSocket y 0 si se cerró.*/
+int recibirHeaderIPC(int unSocket, stHeaderIPC* nuevoPtrAHeader){
+	int resultado, tamanioHeader = sizeof(stHeaderIPC);
+	resultado = recv(unSocket, nuevoPtrAHeader,tamanioHeader,0);
+
+	if(resultado == -1)
+		return -1;
+	return(resultado);
+}
+
+/*----------------------------------------------------------------------------*/
+/*                         Funciones Basicas                                  */
+/*----------------------------------------------------------------------------*/
+/*Envía primero unHeader y luego unContenido por el socket que recibe como parametro. Devuelve 0 si hubieron errores y el tamaño del contenido enviado -sin contar el header- si no.*/
+int enviarMensajeIPCA(int unSocket,stHeaderIPC unHeader, char* unContenido){
+	if (!enviarHeaderIPC(unSocket, unHeader))
+		return(0);
+
+	return(enviarContenidoA(unSocket,unContenido,unHeader.largo));
+}
+
+/*Envía primero unHeader y luego unContenido por el socket que recibe como parametro. Devuelve 0 si hubieron errores y el tamaño del contenido enviado -sin contar el header- si no.*/
+int enviarMensajeIPC(int unSocket,stHeaderIPC unHeader, char* unContenido){
+	int unLargo = strlen(unContenido)+1;
+	unHeader.largo = unLargo;
+	if (!enviarHeaderIPC(unSocket, unHeader))
+		return(0);
+
+	return(enviarContenido(unSocket,unContenido));
+}
+
+/*----------------------------------------------------------------------------*/
+
+/*Devuelve la cantidad recibida -sin contar el header- si se pudo recibir un mensaje atraves de unSocket y 0 si este se cerro.*/
+int recibirMensajeIPC(int unSocket, stMensajeIPC* unNuevoMensaje){
+	if(!(recibirHeaderIPC(unSocket, &(unNuevoMensaje->header))))
+		return(0);
+
+	if (unNuevoMensaje->header.largo > 0 )
+		return(recibirContenido(unSocket,unNuevoMensaje->contenido,unNuevoMensaje->header.largo));
+	else {
+	
+		return 1;
+	}
+}
+
--- Nucleo/src/Nucleo.c
+++ Nucleo/src/Nucleo.c
@@ -1,17 +1,218 @@
 /*
  ============================================================================
  Name        : Nucleo.c
- Author      : 
- Version     :
- Copyright   : Your copyright notice
- Description : Hello World in C, Ansi-style
+ Author      : Jose Maria Suarez
+ Version     : 0.1
+ Description : Elestac - Nucleo
+ ============================================================================
+ */
+#include "../lib/librerias.h"
+#include "../lib/sockets.c"
+#include "../lib/socketsIPCIRC.c"
+#include "../lib/fComunes.c"
+
+/*
+ ============================================================================
+ Estructuras y definiciones
  ============================================================================
  */
 
-#include <stdio.h>
-#include <stdlib.h>
+typedef struct{
+	char* miIP;             /* Mi direccion de IP. Ej: <"127.0.0.1"> */
+	int puertoProg;			/* Puerto de escucha Consola */
+	int sockProg;			/* Socket de escucha Consola */
+	int puertoCpu;			/* Puerto de escucha CPU */
+	int sockCpu;			/* Socket de escucha CPU */
+	int fdMax;              /* Numero que representa al mayor socket de fds_master. */
+	int fdMax2;             /* Numero que representa al mayor socket de fds_master. */
+	int salir;              /* Indica si debo o no salir de la aplicacion. */
+} stEstado;
 
-int main(void) {
-	puts("Core Application"); /* prints Core Application */
-	return EXIT_SUCCESS;
+fd_set fds_master;			/* Lista de todos mis sockets.*/
+fd_set read_fds;	  		/* Sublista de fds_master.*/
+
+
+/*
+ ============================================================================
+ Funciones
+ ============================================================================
+ */
+void loadInfo (stEstado* info,char* file_name){
+
+	t_config* miConf = config_create ("nucleo.conf"); /*Estructura de configuracion*/
+
+	if (config_has_property(miConf,"NUCLEO_IP")) {
+		info->miIP = config_get_string_value(miConf,"NUCLEO_IP");
+	} else {
+		printf("Parametro no cargado en el archivo de configuracion\n \"%s\"  \n","NUCLEO_IP");
+		exit(-2);
+	}
+
+	if (config_has_property(miConf,"PUERTO_PROG")) {
+		info->puertoProg = config_get_int_value(miConf,"PUERTO_PROG");
+	} else {
+		printf("Parametro no cargado en el archivo de configuracion\n \"%s\"  \n","PUERTO_PROG");
+		exit(-2);
+	}
+
+	if (config_has_property(miConf,"PUERTO_CPU")) {
+		info->puertoCpu= config_get_int_value(miConf,"PUERTO_CPU");
+	} else {
+		printf("Parametro no cargado en el archivo de configuracion\n \"%s\"  \n","PUERTO_CPU");
+		exit(-2);
+	}
+}
+
+void cerrarSockets(stEstado *elEstadoActual){
+
+	int unSocket;
+	for(unSocket=3; unSocket <= elEstadoActual->fdMax; unSocket++)
+		if(FD_ISSET(unSocket,&(fds_master)))
+			close(unSocket);
+
+	for(unSocket=3; unSocket <= elEstadoActual->fdMax2; unSocket++)
+		if(FD_ISSET(unSocket,&(fds_master)))
+			close(unSocket);
+
+	FD_ZERO(&(fds_master));
+	FD_ZERO(&(read_fds));
+}
+
+void finalizarSistema(stMensajeIPC *unMensaje,int unSocket, stEstado *unEstado){
+
+	unEstado->salir = 1;
+	unMensaje->header.tipo = -1;
+}
+
+/*
+ ============================================================================
+ Funcion principal
+ ============================================================================
+ */
+int main(int argc, char *argv[]) {
+	stEstado elEstadoActual;
+	stMensajeIPC unMensaje;
+
+	int unClienteConsola = 0, unSocket;
+	int unClienteCpu = 0;
+	struct sockaddr addressAceptado;
+	int maximoAnterior;
+	int agregarSockConsola, agregarSockCpu;
+
+	printf("----------------------------------Elestac------------------------------------\n");
+	printf("-----------------------------------Nucleo------------------------------------\n");
+	printf("------------------------------------v0.1-------------------------------------\n\n");
+	fflush(stdout);
+	/*Carga del archivo de configuracion*/
+	printf("Obteniendo configuracion...");
+	loadInfo(&elEstadoActual,CFGFILE);
+	printf("OK\n");
+
+	/*Inicializacion de listas de socket*/
+	FD_ZERO(&(fds_master));
+	FD_ZERO(&(read_fds));
+
+	/*Inicializacion de sockets de escucha*/
+	elEstadoActual.salir = 0;
+	elEstadoActual.sockCpu= -1;
+	elEstadoActual.sockProg = -1;
+
+	/*Iniciando escucha en el socket escuchador de Consola*/
+	printf("Estableciendo conexion con socket escuchador de Consola...");
+	elEstadoActual.sockProg = escuchar(elEstadoActual.puertoProg);
+	FD_SET(elEstadoActual.sockProg,&(fds_master));
+	elEstadoActual.fdMax =	elEstadoActual.sockProg;
+	printf("OK\n");
+
+	/*Iniciando escucha en el socket escuchador de Consola*/
+	printf("Estableciendo conexion con socket escuchador de CPU...");
+	elEstadoActual.sockCpu = escuchar(elEstadoActual.puertoCpu);
+	FD_SET(elEstadoActual.sockCpu,&(fds_master));
+	elEstadoActual.fdMax2 =	elEstadoActual.sockCpu;
+	printf("OK\n");
+
+	/*Ciclo Principal del Nucleo*/
+	printf(".............................................................................\n");
+	fflush(stdout);
+	printf("..............................Esperando Conexion.............................\n\n");
+	fflush(stdout);
+
+	while(elEstadoActual.salir == 0)
+	{
+		read_fds = fds_master;
+
+		if(seleccionar(elEstadoActual.fdMax,&read_fds,1) == -1){
+			printf("SELECT ERROR - Error Preparando el Select\n");
+			return 1;
+		}
+
+		if(seleccionar(elEstadoActual.fdMax2,&read_fds,1) == -1){
+			printf("SELECT ERROR - Error Preparando el Select\n");
+			return 1;
+		}
+
+		for(unSocket=0;unSocket<=elEstadoActual.fdMax2;unSocket++){
+			unClienteCpu = aceptar(elEstadoActual.sockCpu,&addressAceptado);
+			printf("Nuevo pedido de conexion...\n");
+
+			if(!enviarMensajeIPC(unClienteConsola,nuevoHeaderIPC(QUIENSOS),"MSGQUIENSOS")){
+				printf("No se pudo enviar el MensajeIPC\n");
+			}
+
+			if(!recibirMensajeIPC(unClienteConsola,&unMensaje)){
+				printf("SOCKET_ERROR - No se recibe un mensaje correcto\n");
+				fflush(stdout);
+				close(unClienteConsola);
+			 }
+
+			/*Identifico quien se conecto y procedo*/
+			if(unMensaje.header.tipo=="CONNECTCPU"){
+				if(!enviarMensajeIPC(unClienteCpu,nuevoHeaderIPC(OK),"MSGOK")){
+					printf("No se pudo enviar el MensajeIPC al cliente\n");
+					return 0;
+				}
+
+				printf("Conexion con modulo cliente establecida\n");
+				agregarSockCpu=1;
+
+				/*Agrego el socket conectado A la lista Master*/
+				if(agregarSockCpu==1){
+					FD_SET(unClienteCpu,&(fds_master));
+					if (unClienteCpu > elEstadoActual.fdMax){
+						maximoAnterior = elEstadoActual.fdMax;
+						elEstadoActual.fdMax = unClienteCpu;
+					}
+					agregarSockCpu=0;
+				}
+			}else{
+				memset(unMensaje.contenido,'\0',"LONGITUD_MAX_DE_CONTENIDO");
+
+				if (!recibirMensajeIPC(unSocket,&unMensaje)){
+					if(unSocket==elEstadoActual.sockProg){
+						printf("Se perdio conexion con el cpu conectado...\n ");
+					}
+
+					/*Saco el socket de la lista Master*/
+					FD_CLR(unSocket,&fds_master);
+					close (unSocket);
+
+					if (unSocket > elEstadoActual.fdMax){
+						maximoAnterior = elEstadoActual.fdMax;
+						elEstadoActual.fdMax = unSocket;
+					}
+
+					fflush(stdout);
+
+				}else{
+					/*Agregar en una lista de CPU  */
+				}
+
+			}
+		}
+
+	}
+			cerrarSockets(&elEstadoActual);
+			finalizarSistema(&unMensaje,unSocket,&elEstadoActual);
+			printf("\nNUCLEO: Fin del programa\n");
+			return 0;
 }
