d54ab9ee1223c1a98e96b710c1f9a4f7db7bd5c4
Nicolas A. Pini <nicolas.pini@gmail.com>
2016-04-21 02:02:40 -0300

    Consola version inicial

--- Consola/src/Consola.c
+++ Consola/src/Consola.c
@@ -7,11 +7,105 @@
  Description : Hello World in C, Ansi-style
  ============================================================================
  */
-
 #include <stdio.h>
 #include <stdlib.h>
+#include "consola.h"
 
-int main(void) {
-	puts("Console Application"); /* prints Console Application */
-	return EXIT_SUCCESS;
+int create_console(t_console* tConsole){
+
+	int sockfd;
+
+	if(tConsole)
+	{
+		tConsole->pSockfd = (int*) malloc(sizeof(int));
+		tConsole->tDest_addr = (struct sockaddr_in*) malloc(sizeof(struct sockaddr_in));
+		tConsole->tConfigFile = config_create(PATH_CONFIG);
+	}
+	else
+		return -1;
+
+	if(tConsole->tConfigFile == NULL)
+	{
+		perror("config_create");
+		return -1;
+	}
+	// Valido parametros del archivo de configuracion.
+	if(!config_has_property(tConsole->tConfigFile, PARAM_PORT)
+	|| !config_has_property(tConsole->tConfigFile, PARAM_IP)) {
+		return -1;
+	}
+	// Creo socket para conectar la consola al nucleo
+	if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1){
+		perror("socket");
+		return -1;
+	}
+	*(tConsole->pSockfd) = sockfd;
+
+	// Destination address
+	tConsole->tDest_addr->sin_family = AF_INET;
+	tConsole->tDest_addr->sin_port = htons(config_get_int_value(tConsole->tConfigFile, PARAM_PORT));
+	(tConsole->tDest_addr->sin_addr).s_addr = inet_addr(config_get_string_value(tConsole->tConfigFile, PARAM_IP));
+	memset(tConsole->tDest_addr->sin_zero, '\0', 8);
+
+	return 0;
 }
+
+int connect_console(t_console* tConsole){
+
+	if(!(tConsole->pSockfd) || !(tConsole->tDest_addr)){
+		return -1;
+	}
+	if((connect(*(tConsole->pSockfd), (struct sockaddr *) tConsole->tDest_addr, sizeof(struct sockaddr))) == -1)
+	{
+		perror("connect");
+		return -1;
+	}
+	return 0;
+}
+
+void destroy_console(t_console* tConsole){
+
+	close(*(tConsole->pSockfd));
+
+	if(tConsole->tConfigFile)
+		config_destroy(tConsole->tConfigFile);
+	if(tConsole->pSockfd)
+		free(tConsole->pSockfd);
+	if(tConsole->tDest_addr)
+		free(tConsole->tDest_addr);
+	if(tConsole->pProgram)
+		free(tConsole->pProgram);
+}
+
+int load_program(t_console* tConsole, int argc, char* argv[])
+{
+	FILE* body;
+	char *program = (char*) malloc(10);
+	char buffer[BUFFERSIZE];
+
+	//for(i=0; i< argc; argc++)
+	//	printf("[%d][%s]", i, argv[i]);
+
+	printf("INICIO Programa Ansisop: \n");
+	//body = fopen(argv[0], "r");
+	while(fgets(buffer, BUFFERSIZE, stdin)){
+		program = (char*) realloc( program, strlen(program)+1+strlen(buffer));
+		if(!program)
+			strcat(program, buffer);  /* concatena un salto de linea cada vez */
+		printf("[%s]", *program);
+	}
+	//fclose(body);
+	printf("\nPrograma [%s]\n", *program);
+
+	// Agrego el programa a t_console
+	if(!tConsole)
+		return -1;
+	tConsole->pProgram = program;
+
+	printf("FIN Programa Ansisop: \n");
+	return 0;
+}
+
+
+
+
---
+++ Consola/src/consola_test.c
@@ -0,0 +1,90 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "consola.h"
+#include <sys/wait.h>
+#include <signal.h>
+#define SERVER_TEST_PORT 3990
+#define SERVER_BACKLOG 10
+
+void sigchld_handler(int s)
+{
+	while(wait(NULL) > 0);
+}
+
+int server_test(void)
+{
+	int sockfd, newfd;
+	struct sockaddr_in myAddr;
+	struct sockaddr_in theirAddr;
+	int sin_size;
+	struct sigaction sa;
+	int yes=1;
+	if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
+	{
+		perror("socket server");
+		return -1;
+	}
+	if(setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1) {
+		perror("setsockopt");
+		return -1;
+	}
+	myAddr.sin_family = AF_INET;
+	myAddr.sin_port = htons(SERVER_TEST_PORT);
+	myAddr.sin_addr.s_addr = INADDR_ANY; // ip local
+	memset(&(myAddr.sin_zero), '\0', 8);
+	if (bind(sockfd, (struct sockaddr *)&myAddr, sizeof(struct sockaddr)) == -1) {
+		perror("bind");
+		return -1;
+	}
+	if (listen(sockfd, SERVER_BACKLOG) == -1) {
+	     perror("listen");
+	     return -1;
+	}
+	sa.sa_handler = sigchld_handler; // Eliminar procesos muertos
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = SA_RESTART;
+	if (sigaction(SIGCHLD, &sa, NULL) == -1) {
+		perror("sigaction");
+		return -1;
+	}
+	while(1) {  // main accept() loop
+		sin_size = sizeof(struct sockaddr_in);
+	    if ((newfd = accept(sockfd, (struct sockaddr *)&theirAddr, &sin_size)) == -1){
+	    	perror("accept");
+	    	continue;
+	    }
+	    printf("server: got connection from %s\n", inet_ntoa(theirAddr.sin_addr));
+	    if (!fork())
+	    {
+	    	close(sockfd);
+	        if (send(newfd, "Hello, world!\n", 14, 0) == -1)
+	            perror("send");
+	            close(newfd);
+	            return -1;
+	    	}
+	        close(newfd);
+	    }
+	return 0;
+}
+
+int main(int argc, char *argv[]) {
+	t_console tConsola;
+	load_program(&tConsola, argc, argv);
+	create_console(&tConsola);
+
+	// Para probar la consola como server
+	if(argc > 1)
+	{
+		printf("servermode: \n");
+		if(server_test())
+			printf("error server.. \n");
+	}
+	else
+	{
+		connect_console(&tConsola);
+	}
+
+	destroy_console(&tConsola);
+	puts("Console Application"); /* prints Console Application */
+	return EXIT_SUCCESS;
+}
