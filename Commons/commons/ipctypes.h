122c00337b36793521f36389c4cd75ec2a239b87
josemasuarez <josemasuarez@gmail.com>
2016-05-08 18:57:33 -0300

    Normalizacion de macros comunes de comunicacion

--- Commons/commons/ipctypes.h
+++ Commons/commons/ipctypes.h
@@ -8,6 +8,24 @@
 #ifndef COMMONS_IPCTYPES_H_
 #define COMMONS_IPCTYPES_H_
 
+/*Definicion de Parametros de Conexiones comunes*/
+#define OK					100
+#define ERROR				101
+#define QUIENSOS			102
 
+/*Definicion de Parametros de Consola - Nucleo*/
+#define CONNECTCONSOLA		103
+#define SENDANSISOP         104
+#define IMPRIMIR			105
+#define IMPRIMIRTEXTO		106
+#define SENDPID				107
+#define KILLPID				108
+
+/*Definicion de Parametros de Nucleo - UMC*/
+#define CONNECTNUCLEO		115
+
+/*Definicion de Parametros de Nucleo - CPU*/
+#define CONNECTCPU			104
+#define EXECANSISOP			105
 
 #endif /* COMMONS_IPCTYPES_H_ */
