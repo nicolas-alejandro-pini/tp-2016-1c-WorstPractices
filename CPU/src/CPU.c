f6779f3064784cbfdfdd01fe5304bbc842bd98ec
josemasuarez <josemasuarez@gmail.com>
2016-06-05 20:25:39 -0300

    Se actualizan los archivos

--- CPU/src/CPU.c
+++ CPU/src/CPU.c
@@ -25,13 +25,13 @@
 #include <math.h>
 #include <fcntl.h>
 #include <commons/log.h>
-#include "commons/sockets.h"
+#include <commons/sockets.h>
 #include <commons/socketsIPCIRC.h>
 #include <commons/ipctypes.h>
 #include <commons/pcb.h>
 #include <commons/config.h>
-#include "parser/parser.h"
-#include "parser/metadata_program.h"
+#include <commons/parser/parser.h>
+#include <commons/parser/metadata_program.h>
 
 
 /*Archivos de Configuracion*/
