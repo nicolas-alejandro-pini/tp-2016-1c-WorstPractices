018c3d9dc4d8a8e36dc9c77ac6bf991efae4a6cb
dlaib <djlaib@gmail.com>
2016-06-07 22:57:09 -0300

    Se agrega modularizacion de estructuras UMC (TLB, memoria, Tabla de paginas)

---
+++ UMC/src/TLB.h
@@ -0,0 +1,21 @@
+/*
+ * TLB.h
+ *
+ *  Created on: 7/6/2016
+ *      Author: utnso
+ */
+
+#ifndef TLB_H_
+#define TLB_H_
+
+#include <stdint.h>
+#include "Tablas.h"
+
+/* puntero a la tabla cache TLB */
+char *TLB;
+
+int buscarEnTLB(uint16_t paginaBuscada);
+int reemplazarValorTLB(uint16_t Pagina, stRegistroTablas registro);
+
+
+#endif /* TLB_H_ */
---
+++ UMC/src/TablaMarcos.c
@@ -0,0 +1,15 @@
+/*
+ * TablaMarcos.c
+ *
+ *  Created on: 7/6/2016
+ *      Author: utnso
+ */
+
+int buscarEnTabla(uint16_t paginaBuscada){
+	/* TODO buscarEnTabla */
+	return 0;
+}
+int reemplazarValorTabla(uint16_t Pagina, stRegistroTablas registro){
+	/* TODO reemplazarValorTabla */
+	return 0;
+}
