5d4ad5b2a57d4b955dbc3a735c5b7b832e5e5d08
josemasuarez <josemasuarez@gmail.com>
2016-06-21 14:13:23 -0300

    Comunicacion con Nucleo para inicializar programa

--- UMC/src/ICPU.c
+++ UMC/src/ICPU.c
@@ -16,9 +16,11 @@ void *inicializarPrograma(stIni* ini){
 		unHeader=nuevoHeaderIPC(ERROR);
 		enviarHeaderIPC(ini->socketResp, unHeader);
 		pthread_exit(NULL);
+	}else{
+		crearTabla(ini->sPI->processId, ini->sPI->cantidadPaginas);
+		unHeader=nuevoHeaderIPC(OK);
+		enviarHeaderIPC(ini->socketResp, unHeader);
 	}
-
-	crearTabla(ini->sPI->processId, ini->sPI->cantidadPaginas);
 
 	/* no guardo en memoria */
 
@@ -120,61 +122,61 @@ int estaPaginaDisponible(uint16_t pagina){
 
 
 void realizarAccionCPU(uint16_t socket){
-
-	stMensajeIPC *unMensaje;
-	uint16_t pidActivo;
-	stEnd end;
-
-
-	while(1){
-
-		if(!recibirMensajeIPC(socket, unMensaje)){
-			log_error("Thread Error - No se pudo recibir mensaje de respuesta - socket: %d", socket);
-			liberarHeaderIPC(unMensaje->header);
-			liberarHeaderIPC(unMensaje->contenido);
-			close(socket);
-			pthread_exit(NULL);
-		}
-
-		switch(unMensaje->header.tipo){
-
-		case READ_BTYES_PAGE:
-
-			stPosicion *posR =(stPosicion*)unMensaje->contenido;
-
-			leerBytes(posR, pidActivo, socket);
-
-			break;
-
-		case WRITE_BYTES_PAGE:
-
-			stPosicion *posW =(stPosicion*)unMensaje->contenido;
-
-			escribirBytes(posW, pidActivo, socket);
-
-			break;
-
-		case FINPROGRAMA:
-
-			end = calloc(1,sizeof(stEnd));
-			end->socketResp = socket;
-			end->pid = atoi(unMensaje->contenido);
-
-			finalizarPrograma(end);
-			break;
-
-		case CAMBIOCONTEXTO:
-			/* TODO actualizo tabla de marcos con el pid y TLB flush de ese pid */
-
-
-			break;
-
-		default:
-			log_info("Se recibio una peticion con un codigo desconocido...%i\n", unMensaje->header.tipo);
-			/*enviarMensajeIPC(unSocket,nuevoHeaderIPC(OK),"UMC: Solicitud recibida.");*/
-			/*enviarMensajeIPC(elEstadoActual.sockSwap,nuevoHeaderIPC(OK),"UMC: Confirmar recepcion.");*/
-			break;
-
-		}/*Cierro switch(unMensaje.header.tipo)*/
-	}/*Cierro while*/
+//
+//	stMensajeIPC *unMensaje;
+//	uint16_t pidActivo;
+//	stEnd end;
+//
+//
+//	while(1){
+//
+//		if(!recibirMensajeIPC(socket, unMensaje)){
+//			log_error("Thread Error - No se pudo recibir mensaje de respuesta - socket: %d", socket);
+//			liberarHeaderIPC(unMensaje->header);
+//			liberarHeaderIPC(unMensaje->contenido);
+//			close(socket);
+//			pthread_exit(NULL);
+//		}
+//
+//		switch(unMensaje->header.tipo){
+//
+//		case READ_BTYES_PAGE:
+//
+////			stPosicion *posR =(stPosicion*)unMensaje->contenido;
+//
+//			leerBytes(posR, pidActivo, socket);
+//
+//			break;
+//
+//		case WRITE_BYTES_PAGE:
+//
+//			stPosicion *posW =(stPosicion*)unMensaje->contenido;
+//
+//			escribirBytes(posW, pidActivo, socket);
+//
+//			break;
+//
+//		case FINPROGRAMA:
+//
+//			end = calloc(1,sizeof(stEnd));
+//			end->socketResp = socket;
+//			end->pid = atoi(unMensaje->contenido);
+//
+//			finalizarPrograma(end);
+//			break;
+//
+//		case CAMBIOCONTEXTO:
+//			/* TODO actualizo tabla de marcos con el pid y TLB flush de ese pid */
+//
+//
+//			break;
+//
+//		default:
+//			log_info("Se recibio una peticion con un codigo desconocido...%i\n", unMensaje->header.tipo);
+//			/*enviarMensajeIPC(unSocket,nuevoHeaderIPC(OK),"UMC: Solicitud recibida.");*/
+//			/*enviarMensajeIPC(elEstadoActual.sockSwap,nuevoHeaderIPC(OK),"UMC: Confirmar recepcion.");*/
+//			break;
+//
+//		}/*Cierro switch(unMensaje.header.tipo)*/
+//	}/*Cierro while*/
 }
