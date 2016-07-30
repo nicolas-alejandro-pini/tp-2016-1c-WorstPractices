c3baa692c4b47b9819deccfed85a995e0c1e64f2
martinez-ezequiel <ezequiel.martinez@hotmail.com>
2016-07-30 06:19:08 -0300

    Vuelvo atras los cambios del señales.

--- CPU/src/cpuSignals.c
+++ CPU/src/cpuSignals.c
@@ -11,8 +11,8 @@ t_configCPU *config;
  */
 void signal_handler(int sigNumber)
 {
-  if (sigNumber == SIGUSR1){
-    log_info("Recibida signal de finalizacion SIGUSR1.");
+  if (sigNumber == SIGUSR1 || sigNumber ==SIGINT){
+    log_info("Recibida signal de finalizacion.");
     config->salir = 1;
   }
 }
@@ -26,6 +26,6 @@ void init_signal_handler(t_configCPU *configuracion_inicial){
 
 	if(signal(SIGUSR1, signal_handler) == SIG_ERR)
 		log_error("No pude trapear la signal SIGUSR1");
-//	if(signal(SIGINT, signal_handler) == SIG_ERR)
-//		log_error("No pude trapear la signal SIGINT");
+	if(signal(SIGINT, signal_handler) == SIG_ERR)
+		log_error("No pude trapear la signal SIGINT");
 }
